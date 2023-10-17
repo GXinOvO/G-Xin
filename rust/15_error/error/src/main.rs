905efba53d5e5e294fe2bca1445871e13e13c040
GXinOvO <2758185113@qq.com>
2023-10-17 09:00:38 +0800

    add  rust test

--- rust/15_error/error/src/main.rs
+++ rust/15_error/error/src/main.rs
@@ -9,6 +9,28 @@
     对于可恢复错误用Result<T, E>类来处理，对于不可恢复错误使用panic!宏来处理
  */
 
+use std::fs::File;
+
+fn h(i: i32) -> Result<i32, bool> 
+{
+    if i >= 0 {Ok(i)}
+    else { Err(false) }
+}
+
+fn g(i: i32) -> Result<i32, bool>
+{
+    let t = h(i);
+    return match t {
+        Ok(i) => Ok(i),
+        Err(b) => Err(b)
+    };
+}
+
+fn w(i: i32) -> Result<i32, bool> {
+    let t = h(i)?;
+    Ok(t);  // 因为确定t不是Err,t在这里已经是i32类型
+}
+
  fn main()
  {
      /* 
@@ -21,9 +43,74 @@
  
          RUST_BACKTRACE=1 cargo run
       */
+
+
+    /* 
      panic!("error occured");
      println!("Hello, Rust");
+    */
+    
     /*
         回溯是不可恢复错误的另一种方式，他会展开运行的栈并输出所有的信息，然后程序依然会退出。
       */
- }
+
+
+    /*
+    --TODO: 可恢复的错误
+        此概念十分类似Java编程语言种的异常。实际上在C语言中我们就常常将函数返回值设置成整数
+      来表达函数遇到的错误，在Rust中通过Result<T, E>枚举类作返回值来进行异常表达:
+        enum Result<T, E> 
+        {
+            Ok(T),
+            Err(E),
+        }
+
+        在Rust标准库中可能产生异常的函数的返回值都是Result类型的。
+     */
+    let f = File::open("hello.txt");
+    /*
+        普通版本
+    match f
+    {
+        Ok(file) => {
+            println!("File Opened successfully.");
+        },
+        Err(err) => {
+            println!("Failed to open the file.")
+        }
+    }
+    */
+
+    /* --TODO: 优化 */
+    if let Ok(file) = f {
+        println!("File Opened successfully.");
+    } else {
+        println!("Failed to open the file.");
+    }
+
+    /*
+        --TODO: 如果想使一个可恢复错误按不可恢复错误处理，Result类提供了两个办法: unwrap()和expect(message: &str):
+    let f1 = File::open("hello.txt").unwrap();
+    let f2 = File::open("hello.txt").expect("Failed to open.");
+        相当于在Result为Err时调用panic!宏。两者的区别在于expect能够向panic!宏发送一段指定的错误信息。
+     */
+    let r = h(10000);
+    if let Ok(v) = r {
+        println!("Ok: f(-1) = {}", v);
+    } else {
+        println!("Err");
+    }
+
+    let r1 = g(10000);
+    if let Ok(v) = r1 {
+        println!("Ok: g(10000) = {}", v);
+    } else {
+        println!("Err");
+    }
+
+
+    /*
+        ? 符的实际作用是将Result类非异常的值直接取出,如果有异常就将异常Result返回出去。所以, ? 符仅用于返回值类型为Result<T, E>的函数，其中E类型
+      必须和 ? 所处理的Result的E类型一致。
+     */
+}

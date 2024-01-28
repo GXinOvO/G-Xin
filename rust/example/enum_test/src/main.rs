e5e7f8ec29290aa1ac5bd000db0c4544b011bd20
GXinOvO <2758185113@qq.com>
2024-01-28 16:55:59 +0800

    feat: add rust test

--- rust/example/enum_test/src/main.rs
+++ rust/example/enum_test/src/main.rs
@@ -1,29 +1,21 @@
-enum IpAddrKind
+enum Message 
 {
-    V4,
-    V6,
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
-struct IpAddr 
+impl Message {
+    fn call(&self) {}
+}
+
+fn main()
 {
-    kind: IpAddrKind,
-    address: String,
+    let q = Message::Quit;
+    let m = Message::Move { x: 12, y: 24 };
+    let w = Message::Write(String::from("Hello"));
+    let c = Message::ChangeColor(0, 255, 255);
+
+    m.call(); 
 }
-
-fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    
-}
-
-

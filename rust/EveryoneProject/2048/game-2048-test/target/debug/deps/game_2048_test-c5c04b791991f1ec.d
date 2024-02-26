c56bb2eaa11a7a73bcf5a08c1935c9536bf5cb7f
GXinOvO <2758185113@qq.com>
2024-02-26 15:46:02 +0800

    refactor

--- rust/EveryoneProject/2048/game-2048-test/target/debug/deps/game_2048_test-c5c04b791991f1ec.d
+++
@@ -1,7 +0,0 @@
-/home/bury/JacarandaMimosifolia/rust/study/2048/game-2048-test/target/debug/deps/game_2048_test-c5c04b791991f1ec: src/main.rs src/event.rs src/app.rs
-
-/home/bury/JacarandaMimosifolia/rust/study/2048/game-2048-test/target/debug/deps/game_2048_test-c5c04b791991f1ec.d: src/main.rs src/event.rs src/app.rs
-
-src/main.rs:
-src/event.rs:
-src/app.rs:
